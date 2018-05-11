2cd3cc93e3e0aeca215d07a93d91b275a737b340
Albert Hopkins <albert@rover.com>
2018-05-10 20:14:19 -0700

    fix layout, add setup.py and README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+flister
+-------
+
+A file browser written for Django.
+
+
+To hack on flister:
+
+```
+$ django startproject filebrowser
+$ pip install -e .
+$ cd filebrowser
+```
+
+Then:
+
+    1. Edit `filebrowser/settings.py`
+    2. Add `flister` to `INSTALLED_APPS`
+    3. Change `ROOT_URLCONF` to `flister.urls`
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
+setup(
+    name='flister',
+    version='0.0',
+    description='file browser written in Django',
+    long_description=long_description,
+    author='Albert Hopkins',
+    author_email='marduk@letterboxes.org',
+    packages=find_packages('.'),
+    package_data = {
+        'flister': ['flister/templates/listing.html'],
+    },
+    license='BSD',
+)
